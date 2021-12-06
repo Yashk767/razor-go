6fddb2f21f6faef30e0f13e8abcae4ad92320f66
Yashk767 <yash.kurlekar2017@gmail.com>
2021-12-06 13:13:33 +0530

    Modified functions with accountStruct

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -3,9 +3,6 @@ package accounts
 import (
 	"crypto/ecdsa"
 	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/accounts/keystore"
-	"github.com/ethereum/go-ethereum/crypto"
-	"io/ioutil"
 	"razor/core/types"
 	"razor/logger"
 	"strings"
@@ -13,40 +10,37 @@ import (
 
 var log = logger.NewLogger()
 
-func CreateAccount(path string, password string) accounts.Account {
-	ks := keystore.NewKeyStore(path, keystore.StandardScryptN, keystore.StandardScryptP)
-	accounts.NewManager(&accounts.Config{InsecureUnlockAllowed: false}, ks)
-	newAcc, err := ks.NewAccount(password)
+func CreateAccount(path string, password string, accountUtils AccountUtilsStruct) accounts.Account {
+	newAcc, err := accountUtils.keystoreUtils.NewAccount(path, password)
 	if err != nil {
 		log.Fatal("Error in creating account: ", err)
 	}
 	return newAcc
 }
 
-func getPrivateKeyFromKeystore(keystorePath string, password string) *ecdsa.PrivateKey {
-	jsonBytes, err := ioutil.ReadFile(keystorePath)
+func getPrivateKeyFromKeystore(keystorePath string, password string, accountUtils AccountUtilsStruct) *ecdsa.PrivateKey {
+	jsonBytes, err := accountUtils.razorUtils.ReadFile(keystorePath)
 	if err != nil {
 		log.Fatal("Error in reading keystore: ", err)
 	}
-	key, err := keystore.DecryptKey(jsonBytes, password)
+	key, err := accountUtils.keystoreUtils.DecryptKey(jsonBytes, password)
 	if err != nil {
 		log.Fatal("Error in fetching private key: ", err)
 	}
 	return key.PrivateKey
 }
 
-func GetPrivateKey(address string, password string, keystorePath string) *ecdsa.PrivateKey {
-	ks := keystore.NewKeyStore(keystorePath, keystore.StandardScryptN, keystore.StandardScryptP)
-	allAccounts := ks.Accounts()
+func GetPrivateKey(address string, password string, keystorePath string, accountUtilsStruct AccountUtilsStruct) *ecdsa.PrivateKey {
+	allAccounts := accountUtilsStruct.keystoreUtils.Accounts(keystorePath)
 	for _, account := range allAccounts {
 		if strings.EqualFold(account.Address.Hex(), address) {
-			return getPrivateKeyFromKeystore(account.URL.Path, password)
+			return accountUtilsStruct.accountUtils.getPrivateKeyFromKeystore(account.URL.Path, password, accountUtilsStruct)
 		}
 	}
 	return nil
 }
 
-func Sign(hash []byte, account types.Account, defaultPath string) ([]byte, error) {
-	privateKey := GetPrivateKey(account.Address, account.Password, defaultPath)
-	return crypto.Sign(hash, privateKey)
+func Sign(hash []byte, account types.Account, defaultPath string, accountUtilsStruct AccountUtilsStruct) ([]byte, error) {
+	privateKey := accountUtilsStruct.accountUtils.GetPrivateKey(account.Address, account.Password, defaultPath, accountUtilsStruct)
+	return accountUtilsStruct.cryptoUtils.Sign(hash, privateKey)
 }
