12ac43b4f3fc6c4d5b44e4a12932a6c512489ef7
YashK <yash.kurlekar2017@gmail.com>
2025-03-20 15:30:43 +0530

    refactor: removed another comment

--- core/constants.go
+++ core/constants.go
@@ -29,7 +29,6 @@ const (
 	BlockCompletionTimeout           = 15
 )
 
-//Following are the default config values for all the config parameters
 const (
 	DefaultGasMultiplier    float32 = 1.0
 	DefaultBufferPercent    int32   = 5
